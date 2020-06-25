063ca44b1ffdcef7a2b68a357bbd63d22788abd7
Da-Colon <dcolonantonio@gmail.com>
2020-06-25 00:37:29 -0400

    removed console.log()s

--- routes/csvUpload.js
+++ routes/csvUpload.js
@@ -9,7 +9,6 @@ const {validateCsvData} = require('../controllers/csvValidation')
 
 router.post("/upload-csv", upload.single("file"), function (req, res) {
   const fileRows = [];
-  console.log(req.file)
   csv.parseFile(req.file.path)
   .on("data", function (data) {
       fileRows.push(data);
@@ -21,7 +20,6 @@ router.post("/upload-csv", upload.single("file"), function (req, res) {
         return res.status(403).json({ error: validationError });
       }
       const classList = sortData(fileRows);
-      console.log(classList);
       return res.json({ classList })
     })
 });
